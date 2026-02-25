9e61a6c5f6bce168b59e54ada1fe618b4a268123
Dipti0704 <dipti820h@gmail.com>
2026-02-25 10:05:58 +0530

    implemented streaming

--- Chatbot with UI/langgraph_backend.py
+++ Chatbot with UI/langgraph_backend.py
@@ -1,14 +1,15 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage , HumanMessage
 from langchain_openai import ChatOpenAI
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-llm = ChatOpenAI()
+llm = ChatOpenAI(streaming=True)  # Initialize the language model with streaming enabled
 
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
@@ -27,3 +28,13 @@ graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+# for message_chunk, metadata in chatbot.stream(
+#     {'messages': [HumanMessage(content="what is the recipe to make pasta ")]},
+#     config = {'configurable': {'thread_id': 'thread1'}}, 
+#     stream_mode='messages'
+# ):
+#     if message_chunk.content:
+#         print(message_chunk.content, end=" ", flush=True)
+
+
--- Chatbot with UI/streamlit_frontend_streaming.py
+++ Chatbot with UI/streamlit_frontend_streaming.py
@@ -9,7 +9,7 @@ config = {'configurable': {'thread_id': 'thread1'}}
 ## it gets erase only when you manually refresh the page or close the tab.
 # without this.. every time we press enter the message history will get erased and only the latest message will be there because the whole script will rerun and the message history variable will get reinitialized to empty list.
 if "message_history" not in st.session_state:
-     st.session_state["message_history"] = []
+    st.session_state["message_history"] = []
     
 
 
@@ -27,10 +27,30 @@ if user_input:
     with st.chat_message("user"):
         st.text(user_input)
         
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=config)
-    ai_message = response['messages'][-1].content
-        
+            
     # first add the message to message history 
-    st.session_state["message_history"].append({"role": "assistant", "content": ai_message})    
+    
+    # with st.chat_message("assistant"):
+    #     ai_message = st.write_stream(
+    #         message_chunk.content 
+    #         for message_chunk, metadata in chatbot.stream(
+    #             {'messages': [HumanMessage(content=user_input)]},
+    #             config={'configurable': {'thread_id': 'thread1'}},
+    #             stream_mode='messages'
+    #         )
+            
+    #     )
+    # st.session_state["message_history"].append({"role": "assistant", "content": ai_message})   
+    
+    def generate_response():
+        for message_chunk, metadata in chatbot.stream(
+            {"messages": [HumanMessage(content=user_input)]},
+            config=config,
+            stream_mode="messages",
+        ):
+            if message_chunk.content:
+                yield message_chunk.content
+
     with st.chat_message("assistant"):
-        st.text(f"{ai_message}")
+        ai_message = st.write_stream(generate_response())
+ 
